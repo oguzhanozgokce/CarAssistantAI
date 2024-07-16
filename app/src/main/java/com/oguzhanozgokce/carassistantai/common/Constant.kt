9b8420baaf5bcbb4eadb6f0a0c06ae2a17cab59e
Oguzhan <ozgokceoguzhan34@gmail.com>
2024-07-16 11:37:54 +0300

    Unused classrooms deleted

--- app/src/main/java/com/oguzhanozgokce/carassistantai/common/Constant.kt
+++ app/src/main/java/com/oguzhanozgokce/carassistantai/common/Constant.kt
@@ -2,10 +2,6 @@ package com.oguzhanozgokce.carassistantai.common
 
 
 object Constant {
-
-    const val YOUTUBE_API_KEY = "AIzaSyCmDkEwxItRh7m-mSucXSVc8Cxt3Saq4CA"
-    const val GEMINI_API_KEY = "AIzaSyA7snJmiWJYs-SE1rWD3i5t5OYvbhM80ew"
-
     const val GOOGLE_MAPS_PACKAGE_NAME = "com.google.android.apps.maps"
     const val GOOGLE_MAPS_WEB_URL = "https://www.google.com/maps/dir/?api=1&destination="
     const val INSTAGRAM_PACKAGE_NAME = "com.instagram.android"
@@ -16,5 +12,4 @@ object Constant {
     const val SPOTIFY_WEB_URL = "https://open.spotify.com"
     const val GOOGLE_SEARCH_URL = "https://www.google.com/search?q="
     const val REQUEST_IMAGE_CAPTURE = 1
-
 }
