06e759b78862d1b903e43efe88b9e7eb7c754598
Oguzhan <ozgokceoguzhan34@gmail.com>
2024-07-23 21:00:15 +0300

    Unused classrooms deleted

--- app/src/main/java/com/oguzhanozgokce/carassistantai/ui/chat/utils/app/twitter/TwitterUtils.kt
+++ app/src/main/java/com/oguzhanozgokce/carassistantai/ui/chat/utils/app/twitter/TwitterUtils.kt
@@ -10,9 +10,7 @@ import com.oguzhanozgokce.carassistantai.common.Constant.TWITTER_WEB_URL
 import com.oguzhanozgokce.carassistantai.ui.chat.utils.app.AppUtils
 
 object TwitterUtils {
-    fun openTwitterApp(context: Context, sendBotMessage: (String) -> Unit) {
-        AppUtils.openApp(context, TWITTER_PACKAGE_NAME, TWITTER_WEB_URL, sendBotMessage, R.string.error_opening_twitter_app)
-    }
+
     fun tweet(context: Context, message: String, sendBotMessage: (String) -> Unit) {
        val tweetUrl = TWITTER_SHARE_URL + Uri.encode(message)
         val intent = Intent(Intent.ACTION_VIEW, Uri.parse(tweetUrl)).apply {
